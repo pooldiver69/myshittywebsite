2b32a91959fcaf297a8416aeaf9bce41e4b6ac42
pooldiver69 <38307992+pooldiver69@users.noreply.github.com>
2019-10-23 20:08:48 -0700

    encounter s3 config problem

--- routers/main.js
+++ routers/main.js
@@ -2,10 +2,73 @@ const express = require('express')
 const router = express.Router();
 const path = require('path')
 const Validate = require('../js/authValidate')
+const config = require('../services/aws')
+const AWS = require('aws-sdk');
+const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
+const bucketName = "myshittywebsite"
+const poolData = {
+  UserPoolId: config.cognito.userPoolId,
+  ClientId: config.cognito.clientId
+}
+const poolRegion = config.cognito.region;
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-router.get('/', Validate, function (req, res) {
+var IdentityPoolId = config.IdentityPoolId;
+var idKey = `cognito-idp.${poolRegion}.amazonaws.com/${poolData.UserPoolId}`;
+var cognitoUser = userPool.getCurrentUser();
+function setCredential() {
+
+  if (cognitoUser != null) {
+    cognitoUser.getSession(function (err, result) {
+      if (err) {
+        console.log('Error in getSession()');
+        console.error(err);
+      }
+      if(result) {
+        console.log('User currently logged in.');
+        AWS.config.update({
+          region: poolRegion,
+          credentials: new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: IdentityPoolId,
+            Logins: {[idKey]: result.getIdToken().getJwtToken()}
+          })
+        });
+      }
+    });
+  }
+}
+
+AWS.config.update({
+  region: poolRegion,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IdentityPoolId
+  })
+});
+
+
+router.get('/', function (req, res) {
   console.log(req.session);
   res.sendFile(path.join(__dirname + '/../src/main.html'))
 });
+
+router.post('/upload', function (req, res) {
+  setCredential()
+  var s3 = new AWS.S3({params: {Bucket: bucketName}});
+  let file = req.body.file
+  var params = {
+    Bucket: bucketName,
+    Key: "1234",
+    Body: file
+  };
+
+  s3.upload(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      console.log(data.key + ' successfully uploaded to' + data.Location);
+    }
+  });
+})
+
 
 module.exports = router
--- services/aws.js
+++ services/aws.js
@@ -3,5 +3,11 @@ module.exports =  {
     userPoolId: 'us-east-1_7Sdj2kF5P',
     region: 'us-east-1',
     clientId: '10fprl7u1sh1467jo6a5pjn9ol',
-  }
+  },
+  s3: {
+    accessKeyId: 'AKIAVBW7JFQDHYFYN5LB',
+    secretAccessKey: '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',
+    Bucket: 'onclick',
+  }, 
+  IdentityPoolId: 'us-east-1:621fcfba-9fa1-410d-b246-df860ab8112e',
 }
